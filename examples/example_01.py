5e09f66309094d71949a55ed1eec05f4d2ae2740
Cristóbal Tapia Camú <crtapia@gmail.com>
2020-10-13 18:32:05 +0200

    doc: update example

--- examples/example_01.py
+++ examples/example_01.py
@@ -1,16 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_fill_cmap_between import fill_cmap_between
+from mpl_fill_cmap_between import fill_cmap_between, fill_cmap_between_x
 
-x = np.linspace(-10, 10, 100)
+x = np.linspace(-10, 10, 50)
 y = x**2 - 40
 
 fig = plt.figure(figsize=(4.8, 2.0))
 ax = fig.add_subplot(111)
 
-fill_cmap_between(x, y, 0, ax=ax)
-
-ax.set_aspect("auto")
+fill_cmap_between(x, y, 0, ax=ax, cmap="viridis")
 
 fig.tight_layout()
 fig.savefig("example.png", dpi=300)
+
