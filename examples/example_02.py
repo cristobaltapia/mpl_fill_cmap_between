9b0acbe5356afaef1973cbb62c3c35c571607c79
Cristóbal Tapia Camú <crtapia@gmail.com>
2020-10-14 11:31:21 +0200

    doc: update example

--- examples/example_02.py
+++ examples/example_02.py
@@ -12,6 +12,7 @@ fill_cmap_between(x, y * 0.1, 0, ax=ax, cmap="viridis", kw_line_1=dict(color="k"
                   kw_line_2=dict(color="k", lw=0.5), angle=40, origin=(10, 0))
 ax.set_aspect("equal")
 ax.grid(True, ls=":")
+ax.set_xlim(left=0)
 
 fig.tight_layout()
 fig.savefig("example_02.png", dpi=300)
